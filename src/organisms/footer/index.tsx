893e3f6c94ba4d8ee1540b48f37cf7aef06d33ea
OliveiraGusta <gustavo.oliver456@gmail.com>
2025-01-29 19:42:44 -0300

    refac: cores do footer

--- src/organisms/footer/index.tsx
+++ src/organisms/footer/index.tsx
@@ -4,7 +4,7 @@ import ContactList from "../../atoms/contactList";
 function Footer() {
   return (
     <div style={{
-        background: "radial-gradient(ellipse at right bottom, rgba(6, 5, 21, 0.2) 50%, rgba(255, 255, 255, 1) 500%)"
+        background: "radial-gradient(ellipse at right bottom, #191824 60%, #29236d 100%)"
       }} 
     className="p-10 flex items-center justify-around">
         <p>2025 - Gustavo Oliveira</p>
